d552f1d4c5e4312ccb0da783ed878440019e6733
souhail <souhailbenslimene@outlook.com>
2022-03-28 22:00:04 +0100

    product images animation

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -38,19 +38,19 @@ class Product extends Model
         return route('single-product', ['product' => $this->rowid, 'slug' => Str::slug($this->label)]);
     }
 
-    public function getPhotosAttribute(): \Illuminate\Support\Collection
-    {
-        $dir = env("DOLIBARR_PATH") . '/produit/' . $this->ref;
-        if (is_dir($dir))
-            return collect($this->scanFiles($dir));
-        return collect([]);
-    }
-
     public function getPriceTtcAttribute($value)
     {
         $fmt = new NumberFormatter('fr_FR', NumberFormatter::CURRENCY);
         $fmt->setPattern('#,##0.00 DT');
         return $fmt->formatCurrency($value, 'TND');
+    }
+
+    public function getPhotosAttribute(): \Illuminate\Support\Collection
+    {
+        $dir = env("DOLIBARR_PATH") . '/produit/' . $this->ref;
+        if (is_dir($dir))
+            return collect($this->scanFiles($dir))->values();
+        return collect([]);
     }
 
     private function image_route($dir_element)

7f32f48dde1295d5b57514da2ceed3bfa21f5991
souhail <souhailbenslimene@outlook.com>
2022-03-29 21:15:48 +0100

    no products

--- resources/views/pages/products.blade.php
+++ resources/views/pages/products.blade.php
@@ -13,79 +13,61 @@
                     @include("partials.catalog.sidebar")
                 </div>
                 <div class="col-lg-9 col-md-12">
+                    @if($products->isEmpty())
+                        <div class="shop_toolbar_wrapper">Aucun produit à afficher</div>
+                    @else
                     <!--shop wrapper start-->
-                    @include("partials.catalog.toolbar-top")
+                        @include("partials.catalog.toolbar-top")
 
-                    <div class="row shop_wrapper @if($products->count()>5)grid_4 @else grid_list @endif">
-                        @foreach($products as $product)
-                            <div class="@if($products->count()>5)col-lg-3 col-md-4 col-sm-6 @else col-12 @endif">
-                                <article class="single_product">
-                                    <figure>
-                                        <div class="product_thumb">
-                                            @if($product->photos->isEmpty())
-                                                <a class="primary_img" href="product-details.html"><img
-                                                        src="/assets/img/product/product1.jpg" alt=""></a>
-                                                <a class="secondary_img" href="product-details.html"><img
-                                                        src="/assets/img/product/product2.jpg" alt=""></a>
+                        <div class="row shop_wrapper @if($products->count()>5)grid_4 @else grid_list @endif">
+                            @foreach($products as $product)
+                                <div class="@if($products->count()>5)col-lg-3 col-md-4 col-sm-6 @else col-12 @endif">
+                                    <article class="single_product">
+                                        <figure>
+                                            <div class="product_thumb">
+                                                @if($product->photos->isEmpty())
+                                                    <a class="primary_img" href="product-details.html"><img
+                                                            src="/assets/img/product/product1.jpg" alt=""></a>
+                                                    <a class="secondary_img" href="product-details.html"><img
+                                                            src="/assets/img/product/product2.jpg" alt=""></a>
 
-                                            @else
-                                                <a class="primary_img" href="{{$product->route}}"><img
-                                                        src="{{route("dolibarr", ["file"=>'produit/' . $product->ref . '/' .$product->photos->get(0)])}}"
-                                                        alt=""></a>
-                                                @if($product->photos->get(1))
-                                                    <a class="secondary_img" href="{{$product->route}}"><img
-                                                            src="{{route("dolibarr", ["file"=>'produit/' . $product->ref . '/' .$product->photos->get(1)])}}"
+                                                @else
+                                                    <a class="primary_img" href="{{$product->route}}"><img
+                                                            src="{{route("dolibarr", ["file"=>'produit/' . $product->ref . '/' .$product->photos->get(0)])}}"
                                                             alt=""></a>
+                                                    @if($product->photos->get(1))
+                                                        <a class="secondary_img" href="{{$product->route}}"><img
+                                                                src="{{route("dolibarr", ["file"=>'produit/' . $product->ref . '/' .$product->photos->get(1)])}}"
+                                                                alt=""></a>
+                                                    @endif
                                                 @endif
-                                            @endif
-                                            <div class="label_product">
-                                                @if($product->stock>0)
-                                                    <span class="label_sale">Dispo</span>
-                                                @else
-                                                    <span class="label_out_of_stock">Épuisé</span>
-                                                @endif
+                                                <div class="label_product">
+                                                    @if($product->stock>0)
+                                                        <span class="label_sale">Dispo</span>
+                                                    @else
+                                                        <span class="label_out_of_stock">Épuisé</span>
+                                                    @endif
+                                                </div>
+                                                <div class="action_links">
+                                                    <ul>
+                                                        <li class="wishlist"><a href="wishlist.html"
+                                                                                title="Add to Wishlist"><i
+                                                                    class="fa fa-heart-o" aria-hidden="true"></i></a>
+                                                        </li>
+                                                        <li class="compare"><a href="#" title="compare"><span
+                                                                    class="ion-levels"></span></a></li>
+                                                        <li class="quick_button"><a href="#" data-bs-toggle="modal"
+                                                                                    data-bs-target="#modal_box"
+                                                                                    title="quick view"> <span
+                                                                    class="ion-ios-search-strong"></span></a></li>
+                                                    </ul>
+                                                </div>
+                                                <div class="add_to_cart">
+                                                    <a href="{{route("add-to-cart", ["product"=>$product])}}"
+                                                       title="Ajouter au panier">Ajouter au panier</a>
+                                                </div>
                                             </div>
-                                            <div class="action_links">
-                                                <ul>
-                                                    <li class="wishlist"><a href="wishlist.html"
-                                                                            title="Add to Wishlist"><i
-                                                                class="fa fa-heart-o" aria-hidden="true"></i></a></li>
-                                                    <li class="compare"><a href="#" title="compare"><span
-                                                                class="ion-levels"></span></a></li>
-                                                    <li class="quick_button"><a href="#" data-bs-toggle="modal"
-                                                                                data-bs-target="#modal_box"
-                                                                                title="quick view"> <span
-                                                                class="ion-ios-search-strong"></span></a></li>
-                                                </ul>
-                                            </div>
-                                            <div class="add_to_cart">
-                                                <a href="{{route("add-to-cart", ["product"=>$product])}}" title="Ajouter au panier">Ajouter au panier</a>
-                                            </div>
-                                        </div>
-                                        <div class="product_content grid_content">
-                                            <div class="price_box">
-                                                @if($product->price_min>0)
-                                                    <span class="old_price">{{$product->price_ttc}}</span>
-                                                    <span class="current_price">{{$product->price_min_ttc}}</span>
-                                                @else
-                                                    <span class="current_price">{{$product->price_ttc}}</span>
-                                                @endif
-                                            </div>
-                                            <div class="product_ratings">
-                                                <ul>
-                                                    <li><a href="#"><i class="ion-android-star-outline"></i></a></li>
-                                                    <li><a href="#"><i class="ion-android-star-outline"></i></a></li>
-                                                    <li><a href="#"><i class="ion-android-star-outline"></i></a></li>
-                                                    <li><a href="#"><i class="ion-android-star-outline"></i></a></li>
-                                                    <li><a href="#"><i class="ion-android-star-outline"></i></a></li>
-                                                </ul>
-                                            </div>
-                                            <h3 class="product_name grid_name"><a
-                                                    href="{{$product->route}}">{{$product->label}}</a>
-                                            </h3>
-                                        </div>
-                                        <div class="product_content list_content">
-                                            <div class="left_caption">
+                                            <div class="product_content grid_content">
                                                 <div class="price_box">
                                                     @if($product->price_min>0)
                                                         <span class="old_price">{{$product->price_ttc}}</span>
@@ -94,9 +76,6 @@
                                                         <span class="current_price">{{$product->price_ttc}}</span>
                                                     @endif
                                                 </div>
-                                                <h3 class="product_name"><a
-                                                        href="{{$product->route}}">{{$product->label}}</a>
-                                                </h3>
                                                 <div class="product_ratings">
                                                     <ul>
                                                         <li><a href="#"><i class="ion-android-star-outline"></i></a>
@@ -111,40 +90,74 @@
                                                         </li>
                                                     </ul>
                                                 </div>
-                                                <div class="product_desc">
-                                                    <p>{!! Str::limit($product->description, 220, ' ...') !!}</p>
+                                                <h3 class="product_name grid_name"><a
+                                                        href="{{$product->route}}">{{$product->label}}</a>
+                                                </h3>
+                                            </div>
+                                            <div class="product_content list_content">
+                                                <div class="left_caption">
+                                                    <div class="price_box">
+                                                        @if($product->price_min>0)
+                                                            <span class="old_price">{{$product->price_ttc}}</span>
+                                                            <span
+                                                                class="current_price">{{$product->price_min_ttc}}</span>
+                                                        @else
+                                                            <span class="current_price">{{$product->price_ttc}}</span>
+                                                        @endif
+                                                    </div>
+                                                    <h3 class="product_name"><a
+                                                            href="{{$product->route}}">{{$product->label}}</a>
+                                                    </h3>
+                                                    <div class="product_ratings">
+                                                        <ul>
+                                                            <li><a href="#"><i class="ion-android-star-outline"></i></a>
+                                                            </li>
+                                                            <li><a href="#"><i class="ion-android-star-outline"></i></a>
+                                                            </li>
+                                                            <li><a href="#"><i class="ion-android-star-outline"></i></a>
+                                                            </li>
+                                                            <li><a href="#"><i class="ion-android-star-outline"></i></a>
+                                                            </li>
+                                                            <li><a href="#"><i class="ion-android-star-outline"></i></a>
+                                                            </li>
+                                                        </ul>
+                                                    </div>
+                                                    <div class="product_desc">
+                                                        <p>{!! $product->description !!}</p>
+                                                    </div>
+                                                </div>
+                                                <div class="right_caption">
+                                                    <div class="add_to_cart">
+                                                        <a href="{{route("add-to-cart", ["product"=>$product])}}"
+                                                           title="Ajouter au panier">Ajouter au panier</a>
+                                                    </div>
+                                                    <div class="action_links">
+                                                        <ul>
+                                                            <li class="wishlist"><a href="wishlist.html"
+                                                                                    title="Add to Wishlist"><i
+                                                                        class="fa fa-heart-o"
+                                                                        aria-hidden="true"></i> Add to Wishlist</a></li>
+                                                            <li class="compare"><a href="#" title="compare"><span
+                                                                        class="ion-levels"></span> Compare</a></li>
+                                                            <li class="quick_button"><a href="#" data-bs-toggle="modal"
+                                                                                        data-bs-target="#modal_box"
+                                                                                        title="quick view"> <span
+                                                                        class="ion-ios-search-strong"></span> Quick View</a>
+                                                            </li>
+                                                        </ul>
+                                                    </div>
                                                 </div>
                                             </div>
-                                            <div class="right_caption">
-                                                <div class="add_to_cart">
-                                                    <a href="{{route("add-to-cart", ["product"=>$product])}}" title="Ajouter au panier">Ajouter au panier</a>
-                                                </div>
-                                                <div class="action_links">
-                                                    <ul>
-                                                        <li class="wishlist"><a href="wishlist.html"
-                                                                                title="Add to Wishlist"><i
-                                                                    class="fa fa-heart-o"
-                                                                    aria-hidden="true"></i> Add to Wishlist</a></li>
-                                                        <li class="compare"><a href="#" title="compare"><span
-                                                                    class="ion-levels"></span> Compare</a></li>
-                                                        <li class="quick_button"><a href="#" data-bs-toggle="modal"
-                                                                                    data-bs-target="#modal_box"
-                                                                                    title="quick view"> <span
-                                                                    class="ion-ios-search-strong"></span> Quick View</a>
-                                                        </li>
-                                                    </ul>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </figure>
-                                </article>
-                            </div>
-                        @endforeach
-                    </div>
+                                        </figure>
+                                    </article>
+                                </div>
+                            @endforeach
+                        </div>
 
-                @include("partials.catalog.toolbar-bottom")
-                <!--shop wrapper end-->
+                    @include("partials.catalog.toolbar-bottom")
+                    <!--shop wrapper end-->
                 </div>
+                @endif
             </div>
         </div>
     </div>

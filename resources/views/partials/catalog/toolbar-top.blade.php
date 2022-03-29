fbabda7887dd127208ce5f2e91bd72ae6c5a61bf
souhail <souhailbenslimene@outlook.com>
2022-03-29 21:01:12 +0100

    dynamic grid

--- resources/views/partials/catalog/toolbar-top.blade.php
+++ resources/views/partials/catalog/toolbar-top.blade.php
@@ -3,9 +3,11 @@
     <div class="shop_toolbar_btn">
         <button data-role="grid_3" type="button" class="btn-grid-3" data-bs-toggle="tooltip"
                 title="3"></button>
-        <button data-role="grid_4" type="button" class="btn-grid-4" data-bs-toggle="tooltip"
+        <button data-role="grid_4" type="button" class="btn-grid-4 @if($products->count()>5) active @endif"
+                data-bs-toggle="tooltip"
                 title="4"></button>
-        <button data-role="grid_list" type="button" class="active btn-list" data-bs-toggle="tooltip"
+        <button data-role="grid_list" type="button" class="btn-list @if($products->count()<=5) active @endif"
+                data-bs-toggle="tooltip"
                 title="List"></button>
     </div>
 

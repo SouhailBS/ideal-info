9f8edaf5e642691810886be6c02c28c7d9b29c88
souhail <souhailbenslimene@outlook.com>
2022-03-29 10:56:45 +0100

    mobile menu without children

--- resources/views/partials/menu-mobile.blade.php
+++ resources/views/partials/menu-mobile.blade.php
@@ -1,19 +1,23 @@
 <ul class="offcanvas_main_menu">
     @foreach($categories as $category)
-        <li class="menu-item-has-children">
+        <li @if($category->subCategories->isNotEmpty()) class="menu-item-has-children" @endif>
             <a href="{{$category->route}}">{{$category->label}}</a>
-            <ul class="sub-menu">
-                @foreach($category->subCategories as $subCategory)
-                    <li class="menu-item-has-children">
-                        <a href="{{$subCategory->route}}">{{$subCategory->label}}</a>
-                        <ul class="sub-menu">
-                            @foreach($subCategory->subCategories as $subSubCategory)
-                                <li><a href="{{$subSubCategory->route}}">{{$subSubCategory->label}}</a></li>
-                            @endforeach
-                        </ul>
-                    </li>
-                @endforeach
-            </ul>
+            @if($category->subCategories->isNotEmpty())
+                <ul class="sub-menu">
+                    @foreach($category->subCategories as $subCategory)
+                        <li @if($subCategory->subCategories->isNotEmpty()) class="menu-item-has-children" @endif>
+                            <a href="{{$subCategory->route}}">{{$subCategory->label}}</a>
+                            @if($subCategory->subCategories->isNotEmpty())
+                                <ul class="sub-menu">
+                                    @foreach($subCategory->subCategories as $subSubCategory)
+                                        <li><a href="{{$subSubCategory->route}}">{{$subSubCategory->label}}</a></li>
+                                    @endforeach
+                                </ul>
+                            @endif
+                        </li>
+                    @endforeach
+                </ul>
+            @endif
         </li>
     @endforeach
 </ul>

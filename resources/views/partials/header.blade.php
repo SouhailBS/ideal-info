535f6e1f7a7d98ab0c6d87472b74a99ff6d2cdfe
souhail <souhailbenslimene@outlook.com>
2022-03-29 12:49:22 +0100

    search box categories

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -22,23 +22,11 @@
                     <div class="search_container">
                         <form action="#">
                             <div class="hover_category">
-                                <select class="select_option" name="select" id="categori">
-                                    <option selected value="1">All Categories</option>
-                                    <option value="2">Accessories</option>
-                                    <option value="3">Accessories & More</option>
-                                    <option value="4">Butters & Eggs</option>
-                                    <option value="5">Camera & Video</option>
-                                    <option value="6">Mornitors</option>
-                                    <option value="7">Tablets</option>
-                                    <option value="8">Laptops</option>
-                                    <option value="9">Handbags</option>
-                                    <option value="10">Headphone & Speaker</option>
-                                    <option value="11">Herbs & botanicals</option>
-                                    <option value="12">Vegetables</option>
-                                    <option value="13">Shop</option>
-                                    <option value="14">Laptops & Desktops</option>
-                                    <option value="15">Watchs</option>
-                                    <option value="16">Electronic</option>
+                                <select class="select_option" name="select" id="categories">
+                                    <option selected value="1">Tous les catégories</option>
+                                    @foreach($categories as $category)
+                                        <option value="{{$category->rowid}}">{{$category->label}}</option>
+                                    @endforeach
                                 </select>
                             </div>
                             <div class="search_box">
@@ -114,7 +102,8 @@
                     </div>
 
                     <div class="Offcanvas_footer">
-                        <span><a href="mailto:{{config("company.MAIN_INFO_SOCIETE_MAIL")}}"><i class="fa fa-envelope-o"></i> {{config("company.MAIN_INFO_SOCIETE_MAIL")}}</a></span>
+                        <span><a href="mailto:{{config("company.MAIN_INFO_SOCIETE_MAIL")}}"><i
+                                    class="fa fa-envelope-o"></i> {{config("company.MAIN_INFO_SOCIETE_MAIL")}}</a></span>
                         <ul>
                             <li class="facebook"><a href="#"><i class="fa fa-facebook"></i></a></li>
                             <li class="twitter"><a href="#"><i class="fa fa-twitter"></i></a></li>
@@ -167,23 +156,11 @@
                             <div class="search_container">
                                 <form action="#">
                                     <div class="hover_category">
-                                        <select class="select_option" name="select" id="categori1">
-                                            <option selected value="1">All Categories</option>
-                                            <option value="2">Accessories</option>
-                                            <option value="3">Accessories & More</option>
-                                            <option value="4">Butters & Eggs</option>
-                                            <option value="5">Camera & Video</option>
-                                            <option value="6">Mornitors</option>
-                                            <option value="7">Tablets</option>
-                                            <option value="8">Laptops</option>
-                                            <option value="9">Handbags</option>
-                                            <option value="10">Headphone & Speaker</option>
-                                            <option value="11">Herbs & botanicals</option>
-                                            <option value="12">Vegetables</option>
-                                            <option value="13">Shop</option>
-                                            <option value="14">Laptops & Desktops</option>
-                                            <option value="15">Watchs</option>
-                                            <option value="16">Electronic</option>
+                                        <select class="select_option" name="select" id="categories1">
+                                            <option selected value="1">Tous les catégories</option>
+                                            @foreach($categories as $category)
+                                                <option value="{{$category->rowid}}">{{$category->label}}</option>
+                                            @endforeach
                                         </select>
                                     </div>
                                     <div class="search_box">

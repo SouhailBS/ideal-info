c48379acc77e3630cd17fd8e9d0c7a9ee15bc6c5
souhail <souhailbenslimene@outlook.com>
2022-03-29 20:54:08 +0100

    category products

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -29,7 +29,7 @@ class ProductController extends Controller
 
         if (request()->has("orderby") && isset($sortDisplayValues[request()->get("orderby")])) {
             $order = explode('.', request()->get("orderby"));
-            $products = Product::where('tosell', '>', '0')->orderBy($order[0], $order[1])->paginate(12)->withQueryString();
+            $products = $category->products()->where('tosell', '>', '0')->orderBy($order[0], $order[1])->paginate(12)->withQueryString();
             $sortDisplay .= ": " . $sortDisplayValues[request()->get("orderby")];
         } else
             $products = $category->products()->where('tosell', '>', '0')->paginate(12)->withQueryString();

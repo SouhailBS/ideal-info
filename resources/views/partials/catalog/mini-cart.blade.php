cb1887bdfb89eaab52bc65060a267d96ed93aa3c
souhail <souhailbenslimene@outlook.com>
2022-03-30 13:13:57 +0100

    add mini cart

---
+++ resources/views/partials/catalog/mini-cart.blade.php
@@ -0,0 +1,54 @@
+<!--mini cart-->
+<div class="mini_cart">
+    @if(Cart::isEmpty())
+        <div class="mini_cart_table">
+            <div class="cart_total">
+                <span class="w-100 text-center">Aucun produit dans votre panier</span>
+            </div>
+        </div>
+
+        <div class="mini_cart_footer">
+            <div class="cart_button">
+                <a href="{{route("cart")}}">Voir mon panier</a>
+            </div>
+
+        </div>
+    @else
+        @foreach(Cart::getContent() as $item)
+            <div class="cart_item">
+                <div class="cart_img">
+                    <a href="{{$item->associatedModel->route}}"><img
+                            src="{{route("dolibarr", ["file"=>'produit/' . $item->associatedModel->ref . '/' .$item->associatedModel->photos->get(0)])}}"
+                            alt=""></a>
+                </div>
+                <div class="cart_info">
+                    <a href="{{$item->associatedModel->route}}">{{$item->name}}</a>
+                    <p>Qte: {{$item->quantity}} X <span> {{$item->associatedModel->price_ttc}} </span></p>
+                </div>
+                <div class="cart_remove">
+                    <a href="{{route("delete-from-cart", ["product"=>$item->id])}}"><i
+                            class="ion-android-close"></i></a>
+                </div>
+            </div>
+        @endforeach
+        <div class="mini_cart_table">
+            <div class="cart_total">
+                <span>total:</span>
+                <span
+                    class="price">{{numfmt_format_currency(numfmt_create( 'fr_TN', NumberFormatter::CURRENCY ), Cart::getTotal(), 'TND')}}</span>
+            </div>
+        </div>
+
+        <div class="mini_cart_footer">
+            <div class="cart_button">
+                <a href="{{route("cart")}}">Voir mon panier</a>
+            </div>
+            <div class="cart_button">
+                <a href="checkout.html">Checkout</a>
+            </div>
+
+        </div>
+    @endif
+
+</div>
+<!--mini cart end-->
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -48,56 +48,8 @@
                                     class="fa fa-angle-down"></i></a>
                             @if(!Cart::isEmpty())
                                 <span class="cart_quantity">{{Cart::getContent()->count()}}</span>
-                        @endif
-                        <!--mini cart-->
-                            <div class="mini_cart">
-                                <div class="cart_item">
-                                    <div class="cart_img">
-                                        <a href="#"><img src="/assets/img/s-product/product.jpg" alt=""></a>
-                                    </div>
-                                    <div class="cart_info">
-                                        <a href="#">Sit voluptatem rhoncus sem lectus</a>
-                                        <p>Qty: 1 X <span> $60.00 </span></p>
-                                    </div>
-                                    <div class="cart_remove">
-                                        <a href="#"><i class="ion-android-close"></i></a>
-                                    </div>
-                                </div>
-                                <div class="cart_item">
-                                    <div class="cart_img">
-                                        <a href="#"><img src="/assets/img/s-product/product2.jpg" alt=""></a>
-                                    </div>
-                                    <div class="cart_info">
-                                        <a href="#">Natus erro at congue massa commodo</a>
-                                        <p>Qty: 1 X <span> $60.00 </span></p>
-                                    </div>
-                                    <div class="cart_remove">
-                                        <a href="#"><i class="ion-android-close"></i></a>
-                                    </div>
-                                </div>
-                                <div class="mini_cart_table">
-                                    <div class="cart_total">
-                                        <span>Sub total:</span>
-                                        <span class="price">$138.00</span>
-                                    </div>
-                                    <div class="cart_total mt-10">
-                                        <span>total:</span>
-                                        <span class="price">$138.00</span>
-                                    </div>
-                                </div>
-
-                                <div class="mini_cart_footer">
-                                    <div class="cart_button">
-                                        <a href="{{route("cart")}}">View cart</a>
-                                    </div>
-                                    <div class="cart_button">
-                                        <a href="checkout.html">Checkout</a>
-                                    </div>
-
-                                </div>
-
-                            </div>
-                            <!--mini cart end-->
+                            @endif
+                            @include('partials.catalog.mini-cart')
                         </div>
                     </div>
                     <div id="menu" class="text-left ">
@@ -184,56 +136,8 @@
                                             class="fa fa-angle-down"></i></a>
                                     @if(!Cart::isEmpty())
                                         <span class="cart_quantity">{{Cart::getContent()->count()}}</span>
-                                @endif
-                                <!--mini cart-->
-                                    <div class="mini_cart">
-                                        <div class="cart_item">
-                                            <div class="cart_img">
-                                                <a href="#"><img src="/assets/img/s-product/product.jpg" alt=""></a>
-                                            </div>
-                                            <div class="cart_info">
-                                                <a href="#">Sit voluptatem rhoncus sem lectus</a>
-                                                <p>Qty: 1 X <span> $60.00 </span></p>
-                                            </div>
-                                            <div class="cart_remove">
-                                                <a href="#"><i class="ion-android-close"></i></a>
-                                            </div>
-                                        </div>
-                                        <div class="cart_item">
-                                            <div class="cart_img">
-                                                <a href="#"><img src="/assets/img/s-product/product2.jpg" alt=""></a>
-                                            </div>
-                                            <div class="cart_info">
-                                                <a href="#">Natus erro at congue massa commodo</a>
-                                                <p>Qty: 1 X <span> $60.00 </span></p>
-                                            </div>
-                                            <div class="cart_remove">
-                                                <a href="#"><i class="ion-android-close"></i></a>
-                                            </div>
-                                        </div>
-                                        <div class="mini_cart_table">
-                                            <div class="cart_total">
-                                                <span>Sub total:</span>
-                                                <span class="price">$138.00</span>
-                                            </div>
-                                            <div class="cart_total mt-10">
-                                                <span>total:</span>
-                                                <span class="price">$138.00</span>
-                                            </div>
-                                        </div>
-
-                                        <div class="mini_cart_footer">
-                                            <div class="cart_button">
-                                                <a href="{{route("cart")}}">Voir mon panier</a>
-                                            </div>
-                                            <div class="cart_button">
-                                                <a href="checkout.html">Checkout</a>
-                                            </div>
-
-                                        </div>
-
-                                    </div>
-                                    <!--mini cart end-->
+                                    @endif
+                                    @include('partials.catalog.mini-cart')
                                 </div>
                             </div>
                         </div>

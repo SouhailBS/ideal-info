8193e64d455d01e7f0fde4b9a4bb0c5850ed4eea
souhail <souhailbenslimene@outlook.com>
2022-04-08 15:30:33 +0100

    reduce products count

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -16,9 +16,9 @@ class HomeController extends Controller
     {
         $products = Product::where('tosell', '>', '0')->where('fk_product_type',0)->inRandomOrder()->take(20)->get();
         $categories = Category::where('visible', '0')->where('fk_parent', '2')->take(6)->get();
-        $bestSeller = Category::where("rowid", 5)->first()->products()->where('tosell', '>', '0')->take(20)->get();
-        $newProducts = Product::where('tosell', '>', '0')->where('fk_product_type',0)->where('datec', '>', now()->subDays(30)->endOfDay())->inRandomOrder()->take(30)->get();
-        $promo = Product::where('tosell', '>', '0')->where('price_min', '>', 0)->where('stock', '>', 0)->inRandomOrder()->take(30)->get();
+        $bestSeller = Category::where("rowid", 5)->first()->products()->where('tosell', '>', '0')->take(12)->get();
+        $newProducts = Product::where('tosell', '>', '0')->where('fk_product_type',0)->where('datec', '>', now()->subDays(30)->endOfDay())->inRandomOrder()->take(20)->get();
+        $promo = Product::where('tosell', '>', '0')->where('price_min', '>', 0)->where('stock', '>', 0)->inRandomOrder()->take(20)->get();
         return view("home")->with([
             "products" => $products,
             "bestSeller" => $bestSeller,
